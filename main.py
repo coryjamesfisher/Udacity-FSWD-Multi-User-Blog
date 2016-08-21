ebda51b04b929ce0a0e230f1546156e3bea6958b
Cory Fisher <coryjamesfisher@gmail.com>
2016-08-21 16:51:25 -0400

    Implemented many features. TODO Document them

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import os
 import jinja2
 import webapp2
 import hmac
+import urllib
+from markupsafe import Markup
 
 from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+def urlencode_filter(s):
+    if type(s) == 'Markup':
+        s = s.unescape()
+    s = s.encode('utf8')
+    s = urllib.quote_plus(s)
+    return Markup(s)
+
+jinja_env.filters['urlencode'] = urlencode_filter
 SECRET = "you'll never get it out of me"
 
 class Handler(webapp2.RequestHandler):
@@ -15,7 +26,7 @@ class Handler(webapp2.RequestHandler):
 
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
-        return t.render(params)
+        return t.render(dict(params, user=self.user))
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
@@ -36,6 +47,9 @@ class Handler(webapp2.RequestHandler):
         cookie_val = self.request.cookies.get(name)
         return cookie_val and self.check_secure_val(cookie_val)
 
+    def delete_cookie(self, name):
+        self.response.headers.add_header('Set-Cookie', name + "=;")
+
     def initialize(self, *a, **kw):
         webapp2.RequestHandler.initialize(self, *a, **kw)
         username = self.read_secure_cookie('username')
@@ -45,50 +59,139 @@ class Handler(webapp2.RequestHandler):
 class PostListHandler(Handler):
     def get(self):
 
+        ancestor = None
+        pageTitle = "All Posts - All Authors"
+
+        if self.request.get("owner", ""):
+            ancestor = ndb.Key(User, self.request.get("owner"))
+            pageTitle = "All Posts - " + self.request.get("owner")
+            
+
+        query = Post.query(ancestor = ancestor).order(-Post.created)
+        posts = query.fetch(10, offset = 0)
+        self.render("list.html", posts=posts, pageTitle = pageTitle)
+
+
+class PostHandler(Handler):
+    def get(self):
+        post = None
+        comments = None
+
         if not self.user:
             print "No User - Access Denied"
             return
-        
-        posts = [{"title": "Why is cory so cool? <marquee>HAHAHA</marquee>"}, {"title": "Cory Is So Awesome!"}]
-        self.render("list.html", coolname="CORY", posts=posts)
 
-class UserHandler(Handler):
-    def get(self):
-        self.render("register.html", userForm = UserForm(None))
+        if self.request.get("post_key", "") != "":
+            postKey = ndb.Key(urlsafe = self.request.get("post_key"))
+            post = postKey.get()
+            comments = Comment.query(ancestor = postKey).fetch(100)
 
-    def post(self):
-        userForm = UserForm(self.request.POST)
-
-        if not userForm.validate():
-		self.render("register.html", userForm = userForm) 
+        if not self.request.get("edit", ""):
+            if not post:
+                self.redirect('/posts')
                 return
 
-	userForm.toModel().put()
-        self.make_secure_cookie('username', userForm.username)
-        self.redirect('/posts')
-
-class AuthHandler(Handler):
-    def get(self):
-        self.render("auth.html", authForm = AuthForm(None))
-
-    def post(self):
-        authForm = AuthForm(self.request.POST)
-
-        if not authForm.validate():
-            self.render("auth.html", authForm = authForm)
+            self.render("post/view.html", post = post, comments = comments)
             return
 
-        user = User.query(User.username == authForm.username, User.password == hmac.new(SECRET, authForm.password).hexdigest()).get()
+        postForm = PostForm(None)
+
+        if post and post.owner == self.user.username:
+	    postForm.post_key = post.key.urlsafe()
+            postForm.title = post.title
+            postForm.content = post.content
+
+        self.render("post/edit.html", postForm = postForm)
+
+    def post(self):
+        postForm = PostForm(self.request.POST)
+
+        if not postForm.validate():
+            self.render("post/edit.html", postForm = postForm)
+            return
+
+        if postForm.post_key:
+            postKey = ndb.Key(urlsafe = self.request.get("post_key"))
+            post = postKey.get()
+
+            if post.owner != self.user.username:
+                # todo add error message
+                self.redirect('/post?post_key=' + post.key.urlsafe())
+                return
+
+        else:
+            post.owner = self.user.username
+            postId = ndb.Model.allocate_ids(size=1, parent=self.user.key)[0]
+            post.key = ndb.Key("Post", postId, parent=self.user.key)
+
+        post.title = postForm.title 
+        post.content = postForm.content
+        post.put()
+        self.redirect('/post?post_key=' + post.key.urlsafe())
+
+
+class CommentHandler(Handler):
+    def post(self):
+        commentForm = CommentForm(self.request.POST)
+
+        if not commentForm.validate():
+
+            print "crappy error validating comment form"
+            return
+
+        comment  = commentForm.toModel()
+        comment.owner = self.user.username
+
+	postKey = ndb.Key(urlsafe = commentForm.post)
+        commentId = ndb.Model.allocate_ids(size=1, parent=postKey)[0]
+        comment.key = ndb.Key("Comment", commentId, parent=postKey)
+
+        comment.put()
+        self.redirect('/post?post_key=' + commentForm.post)
+
+
+class RegisterHandler(Handler):
+    def get(self):
+        self.render("register.html", registerForm = RegisterForm(None))
+
+    def post(self):
+        registerForm = RegisterForm(self.request.POST)
+
+        if not registerForm.validate():
+		self.render("register.html", registerForm = registerForm) 
+                return
+
+	registerForm.toModel().put()
+        self.make_secure_cookie('username', registerForm.username)
+        self.redirect('/posts')
+
+class LoginHandler(Handler):
+    def get(self):
+        self.render("login.html", loginForm = LoginForm(None))
+
+    def post(self):
+        loginForm = LoginForm(self.request.POST)
+
+        if not loginForm.validate():
+            self.render("login.html", loginForm = loginForm)
+            return
+
+        user = User.query(User.username == loginForm.username, User.password == hmac.new(SECRET, loginForm.password).hexdigest()).get()
 
         if not user:
-            authForm.username_error = "Incorrect username or password"
-            self.render("auth.html", authForm = authForm)
+            loginForm.username_error = "Incorrect username or password"
+            self.render("login.html", loginForm = loginForm)
         else:
             self.make_secure_cookie('username', user.username)
             self.redirect('/posts')
 
+class LogoutHandler(Handler):
+    def get(self):
+        self.delete_cookie('username')
+        self.redirect('/login')
+
 # FORMS
-class UserForm:
+class RegisterForm:
     def __init__(self, post_data):
         self.username_error = ""
         self.email_error = ""
@@ -129,7 +232,7 @@ class UserForm:
     def toModel(self):
         return User(username = self.username, email = self.email, password = hmac.new(SECRET, self.password).hexdigest(), id = self.username)
 
-class AuthForm:
+class LoginForm:
     def __init__(self, post_data):
 
         self.username_error = ""
@@ -154,6 +257,58 @@ class AuthForm:
 
         return valid
 
+class PostForm:
+    def __init__(self, post_data):
+
+        self.title = ""
+        self.content = ""
+        self.post_key = ""
+        self.title_error = ""
+        self.content_error = ""
+        
+        if post_data is not None:
+            self.title = post_data.get("title", "")
+            self.content = post_data.get("content", "")
+            self.post_key = post_data.get("post_key", "")
+
+    def validate(self):
+
+        valid = True
+        if self.title == "":
+            self.title_error = "Enter a title"
+            valid = False
+        if self.content == "":
+            self.content_error = "Enter some content"
+            valid = False
+
+        return valid
+
+class CommentForm:
+    def __init__(self, post_data):
+
+        self.content = "" 
+        self.post = ""
+        self.content_error = ""
+        self.general_error = ""
+
+        if post_data is not None:
+            self.content = post_data.get("content", "")
+            self.post = post_data.get("post", "")
+
+    def validate(self):
+
+        valid = True
+        if self.content == "":
+            self.content_error = "Enter a comment"
+            valid = False
+        if self.post == "":
+            self.general_error = "A system error has occured. Please try again later"
+
+        return valid
+
+    def toModel(self):
+        return Comment(content = self.content, post = self.post)
+
 # MODELS
 class User(ndb.Model):
     username = ndb.StringProperty(required = True)
@@ -162,4 +317,19 @@ class User(ndb.Model):
     created = ndb.DateTimeProperty(auto_now_add = True)
     modified = ndb.DateTimeProperty(auto_now = True)
 
-app = webapp2.WSGIApplication([('/posts', PostListHandler), ('/users', UserHandler), ('/auth', AuthHandler)], debug=True)
+class Post(ndb.Model):
+    title = ndb.StringProperty(required = True)
+    content = ndb.TextProperty(required = True)
+    owner = ndb.StringProperty(required = True)
+    created = ndb.DateTimeProperty(auto_now_add = True)
+    modified = ndb.DateTimeProperty(auto_now = True)
+
+class Comment(ndb.Model):
+    content = ndb.TextProperty(required = True)
+    owner = ndb.StringProperty(required = True)
+    post = ndb.StringProperty(required = True)
+    created = ndb.DateTimeProperty(auto_now_add = True)
+    modified = ndb.DateTimeProperty(auto_now = True)
+
+
+app = webapp2.WSGIApplication([('/posts', PostListHandler), ('/post', PostHandler), ('/register', RegisterHandler), ('/login', LoginHandler), ('/logout', LogoutHandler), ('/comment', CommentHandler)], debug=True)
